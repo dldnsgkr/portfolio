5a3b45354fb823e0849edc35e9d6e6b32f84d95b
Lee UnHak <uhlee@cyglobal.net>
2026-01-22 02:16:31 +0900

    Feat: 회사 프로젝트 skill list 정리

--- src/data/projectList.ts
+++ src/data/projectList.ts
@@ -9,6 +9,13 @@ export const projectListData: ProjectWrapperType[] = [
       "ajax를 활용해 동적으로 데이터를 받아와 처리하는 작업",
       "막대 차트 및 도넛, 라인등의 여러 혼합차트를 작업 했습니다",
     ],
+    stackList: ["html", "css", "chart.js", "javascript"],
+    troubleShooting: [
+      {
+        title: "",
+        content: "",
+      },
+    ],
   },
   {
     title: "중소기업기술정보진흥원 지능형 추천시스템 개발",
@@ -16,6 +23,13 @@ export const projectListData: ProjectWrapperType[] = [
     contentText: [
       "selenium을 활용한 크롤링 작업을 했습니다.",
       "크롤링한 데이터를 가공하여 엑셀로 저장하는 작업을 했습니다.",
+    ],
+    stackList: ["python", "selenium"],
+    troubleShooting: [
+      {
+        title: "",
+        content: "",
+      },
     ],
   },
   {
@@ -25,6 +39,20 @@ export const projectListData: ProjectWrapperType[] = [
       "next.js 기반 web view 화면 작업했습니다.",
       "toss payments, kgmobiliance(결제 및 본인인증) 연동 작업했습니다.",
     ],
+    stackList: [
+      "tosspaymentsSDK",
+      "next.js",
+      "typescript",
+      "swr",
+      "zustand",
+      "tailwindcss",
+    ],
+    troubleShooting: [
+      {
+        title: "",
+        content: "",
+      },
+    ],
   },
   {
     title: "Hectofinancial",
@@ -33,11 +61,37 @@ export const projectListData: ProjectWrapperType[] = [
       "react 기반의 공통컴포넌트 구축 작업을 했습니다.",
       "UI 특성 별로 layout component를 구분",
     ],
+    stackList: [
+      "next.js",
+      "typescript",
+      "swr",
+      "zustand",
+      "tailwindcss",
+      "shadcn",
+      "react-hook-form",
+      "zod",
+      "react-quill",
+    ],
   },
   {
     title: "etevers",
     description: "",
     contentText: ["react 기반의 공통컴포넌트 구축 작업을 했습니다."],
+    stackList: [
+      "react",
+      "typescript",
+      "tanstack-router",
+      "tanstack-query",
+      "zustand",
+      "tailwindcss",
+      "shadcn",
+      "react-hook-form",
+      "zod",
+      "react-quill",
+      "axios",
+      "framer-motion",
+      "ag-grid",
+    ],
   },
   {
     title: "엔지니어링공제",
@@ -46,6 +100,7 @@ export const projectListData: ProjectWrapperType[] = [
     contentText: [
       "exbuilder라는 전자정부 프레임워크를 사용해 퍼블리싱 및 공통 컴포넌트 작업을 했습니다.",
     ],
+    stackList: ["java", "exbuilder", "javascript(es5)"],
   },
   {
     title: "SRT",
@@ -57,6 +112,7 @@ export const projectListData: ProjectWrapperType[] = [
       "웹 접근성(공공기관 기준)을 준수하여 작업했습니다.",
       "퍼블리싱 된 html 화면을 jsp로 변환 및 백엔드(spring boot)와 연동 작업을 했습니다.",
     ],
+    stackList: ["html", "css", "javascript", "ajax", "java", "spring boot"],
   },
 ];
 
